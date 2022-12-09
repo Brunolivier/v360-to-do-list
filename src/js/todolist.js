e7c9c4dfa79ec2ede8487b6e6ea9a924f1ad5ef4
Bruno Oliveira <bpereiraoliveira@gmail.com>
2022-12-09 15:45:34 -0300

    Adicionado a tarefa de limpar o texto ao adicionar uma nova tarefa.

--- src/js/todolist.js
+++ src/js/todolist.js
@@ -1,6 +1,3 @@
-
-
-
 let bancoDados = [
     {'tarefa':'Estudar JS', 'status': ''},
     {'tarefa':'Estudar CSS', 'status': ''},
@@ -8,22 +5,38 @@ let bancoDados = [
 ];
 
 
-const criarItem = (tarefa, status) => {
+const criarItem = (tarefa, status = '') => {
     const item = document.createElement('label');
     item.classList.add('item');
-    item.innerHTML = 
-        `
-        <input type="checkbox" ${status}>
+    item.innerHTML = `<input type="checkbox" ${status}>
         <div>${tarefa}</div>
         <input type="button" value="">
         `
     document.getElementById('todoList').appendChild(item);
 }
 
-
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while(todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild)
+    }
+}
 
 const atualizarPagina = () => {
-    criarItem('teste1');
+    limparTarefas();
+    bancoDados.forEach(item => criarItem(item.tarefa, item.status));
 }
+
+const adicionarItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if(tecla === 'Enter') {
+    bancoDados.push({'tarefa': texto, 'status': ''});
+    atualizarPagina();
+    evento.target.value = ''; //limpa a tarefa
+    }
+}
+
+document.getElementById('newItem').addEventListener('keypress', adicionarItem);
 
 atualizarPagina();
