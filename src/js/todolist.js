7c2d0a0070ff1410a6af1c36b7a3ee875b20d77f
Bruno Oliveira <bpereiraoliveira@gmail.com>
2022-12-09 23:54:43 -0300

    atualizado as funções de criar e remover item

--- src/js/todolist.js
+++ src/js/todolist.js
@@ -5,12 +5,12 @@ let bancoDados = [
 ];
 
 
-const criarItem = (tarefa, status = '') => {
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
     item.classList.add('item');
-    item.innerHTML = `<input type="checkbox" ${status}>
+    item.innerHTML = `<input type="checkbox" ${status} data-indice=${indice}>
         <div>${tarefa}</div>
-        <input type="button" value="">
+        <input type="button" value="X" data-indice=${indice}>
         `
     document.getElementById('todoList').appendChild(item);
 }
@@ -24,7 +24,7 @@ const limparTarefas = () => {
 
 const atualizarPagina = () => {
     limparTarefas();
-    bancoDados.forEach(item => criarItem(item.tarefa, item.status));
+    bancoDados.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
 }
 
 const adicionarItem = (evento) => {
@@ -37,6 +37,34 @@ const adicionarItem = (evento) => {
     }
 }
 
+const removerItem = (indice) => {
+    bancoDados.splice (indice, 1);
+    atualizarPagina();
+
+}
+
+
+const atualizarItem = (indice) => {
+    bancoDados[indice].status = bancoDados[indice].status === '' ? 'checked' : '';
+    atualizarPagina();
+
+}
+
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem(indice);
+    }
+    else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+        atualizarItem(indice);
+    }
+}
+
 document.getElementById('newItem').addEventListener('keypress', adicionarItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
 
 atualizarPagina();
